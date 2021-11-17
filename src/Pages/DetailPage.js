f8f7631af2df91db5813d552d7fa6fd079b8e68b
ps-rashid <rashid@purgesol.com>
2021-11-17 18:26:48 +0500

    Update DetailPage.js

--- src/Pages/DetailPage.js
+++ src/Pages/DetailPage.js
@@ -14,7 +14,7 @@ const DeatailPage = () => {
             <div className="container">
                 <div className="nav-detail">
                     <div className="row">
-                        <div className="col-6">
+                        <div className="col-5">
                             <div className="prod">
                                 <div>
                                     <img src={sym} />
@@ -25,7 +25,7 @@ const DeatailPage = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="col-6">
+                        <div className="col-7">
                             <nav className="navbar navbar-expand-lg">
                                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                     <span className="navbar-toggler-icon"></span>
